8ba52cc89bfcf618d354868b70b60e2c4d4b57b0
zhh <3286252390@qq.com>
2020-05-21 16:36:56 +0800

    button

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,14 +1,14 @@
 // 什么是单元测试
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
-import Button, { ButtonProps } from './button'
+import Button, { ButtonProps, ButtonType, ButtonSize } from './button'
 const defaultProps = {
   onClick: jest.fn()
 }
 
 const testProps: ButtonProps = {
-  btnType: 'primary',
-  size: 'lg',
+  btnType: ButtonType.Primary,
+  size: ButtonSize.Large,
   className: 'klass'
 }
 
@@ -34,7 +34,7 @@ describe('test Button component', () => {
     expect(element).toHaveClass('btn-primary btn-lg klass')
   })
   it('should render a link when btnType equals link and href is provided', () => {
-    const wrapper = render(<Button btnType='link' href="http://dummyurl">Link</Button>)
+    const wrapper = render(<Button btnType={ButtonType.Link} href="http://dummyurl">Link</Button>)
     const element = wrapper.getByText('Link')
     expect(element).toBeInTheDocument()
     expect(element.tagName).toEqual('A')
