a83a7196b70eaba50285771fef8af260dd02b11f
周红红 <honghong.zhou@ly.com>
2020-07-07 15:24:32 +0800

    Icon

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,35 @@ import Alert, { AlertType } from './components/Alert/alert'
 import Tab from './components/Tab/tab'
 import TabItem from './components/Tab/tabItem'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+import Icon from './components/Icon/icon'
+
 function App() {
   return (
     <div className="App">
+      <p> Font Awesome</p>
+      {/* Font Awesome */}
+      <FontAwesomeIcon icon={faCoffee} size='10x' />
+      <p> Menu </p>
       {/* Menu */}
       <Menu defaultIndex={0} onSelect={(index) => {alert(index)}}>
         <MenuItem index={0}>cool link1</MenuItem>
         <MenuItem index={1} disabled>cool link2</MenuItem>
         <MenuItem index={2}>cool link3</MenuItem>
       </Menu>
+      <p>Button</p>
       {/* Button */}
       <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
       <Button btnType={ButtonType.Link} href='http://www.baidu.com'>Baidu Link</Button>
-
+      <p>Alert</p>
       {/* Alert */}
       <Alert title="alert title" type={AlertType.Default}></Alert>
       <Alert title='title' type={AlertType.Success} description='a paragraph description'></Alert>
       <Alert title="alert title" type={AlertType.Danger}></Alert>
       <Alert title="alert title" type={AlertType.Warning}></Alert>
-
+      <p>Tab</p>
       {/* Tab */}
       <Tab defaultIndex={0}>
         <TabItem index={0} label="Tab one">
@@ -40,6 +50,9 @@ function App() {
           Tab3 Content
         </TabItem>
       </Tab>
+      <p>Icon</p>
+      {/* Icon */}
+      <Icon icon={faCoffee} theme='danger' size='6x'></Icon>
     </div>
   );
 }
